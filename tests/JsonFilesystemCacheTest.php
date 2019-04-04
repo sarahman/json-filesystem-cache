219c043567db83eab25d2db99c37c353e6670684
Syed Abidur Rahman <aabid048@gmail.com>
2019-04-04 06:40:12 +0600

    Cache functionality tests of a custom file created in custom directory are added.

--- tests/JsonFilesystemCacheTest.php
+++ tests/JsonFilesystemCacheTest.php
@@ -33,4 +33,32 @@ class JsonFilesystemCacheTest extends \PHPUnit\Framework\TestCase
         $this->assertArrayHasKey('sample', $cache->get('your_custom_key'));
         $this->assertArrayHasKey('another', $cache->get('your_custom_key'));
     }
+
+    /**
+     * @test
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     * @throws \Exception
+     */
+    public function it_checks_data_stored_in_the_given_directory()
+    {
+        $cache = new JSONFileSystemCache('cache.json', 'tmp');
+
+        $this->assertInstanceOf('Sarahman\SimpleCache\JSONFileSystemCache', $cache);
+
+        $cache->clear();
+
+        $this->assertFalse($cache->has('your_custom_key'));
+
+        // Set Cache key.
+        $cache->set('your_custom_key', [
+            'sample' => 'data',
+            'another' => 'data'
+        ]);
+
+        $this->assertTrue($cache->has('your_custom_key'));
+
+        // Get Cached key data.
+        $this->assertArrayHasKey('sample', $cache->get('your_custom_key'));
+        $this->assertArrayHasKey('another', $cache->get('your_custom_key'));
+    }
 }
